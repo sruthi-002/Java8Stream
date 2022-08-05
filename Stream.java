e04d40fee12c446c94f844f31674d579feac6aef
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-05 16:58:53 +0530

    Add files via upload

---
+++ Stream.java
@@ -0,0 +1,17 @@
+﻿package com.company;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner in = new Scanner(System.in);
+        int n ,i;
+        n = in.nextInt();
+        for(i=0;i<n;i++)
+            list.add(in.next());
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
+    }
+}
